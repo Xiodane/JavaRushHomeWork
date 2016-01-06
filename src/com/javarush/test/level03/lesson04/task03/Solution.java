f461e78381834184e72000df2334c81b29b92ffd
Xiodane <Ilja098@mail.ru>
2016-01-06 20:41:26 +0200

    after

--- src/com/javarush/test/level03/lesson04/task03/Solution.java
+++ src/com/javarush/test/level03/lesson04/task03/Solution.java
@@ -9,8 +9,60 @@ public class Solution
 {
     public static void main(String[] args)
     {
-        //напишите тут ваш код
-
+        Zerg vasja = new Zerg();
+        Zerg vasja1 = new Zerg();
+        Zerg vasja2 = new Zerg();
+        Zerg vasja3 = new Zerg();
+        Zerg vasja4 = new Zerg();
+        Zerg vasja5 = new Zerg();
+        Zerg vasja6 = new Zerg();
+        Zerg vasja7 = new Zerg();
+        Zerg vasja8 = new Zerg();
+        Zerg vasja9 = new Zerg();
+        Protos vasja11 = new Protos();
+        Protos vasja12 = new Protos();
+        Protos vasja13 = new Protos();
+        Protos vasja14 = new Protos();
+        Protos vasja15 = new Protos();
+        Terran vasja16 = new Terran();
+        Terran vasja17 = new Terran();
+        Terran vasja18 = new Terran();
+        Terran vasja19 = new Terran();
+        Terran vasja20 = new Terran();
+        Terran vasja21 = new Terran();
+        Terran vasja22 = new Terran();
+        Terran vasja23 = new Terran();
+        Terran vasja24 = new Terran();
+        Terran vasja25 = new Terran();
+        Terran vasja26 = new Terran();
+        Terran vasja27 = new Terran();
+        vasja.name = "v1";
+        vasja1.name = "v2";
+        vasja2.name = "v3";
+        vasja3.name = "v4";
+        vasja4.name = "v5";
+        vasja5.name = "v6";
+        vasja6.name = "v7";
+        vasja7.name = "v8";
+        vasja8.name = "v9";
+        vasja9.name = "v10";
+        vasja11.name = "v11";
+        vasja12.name = "v12";
+        vasja13.name = "v13";
+        vasja14.name = "v14";
+        vasja15.name = "v15";
+        vasja16.name = "v16";
+        vasja17.name = "v17";
+        vasja18.name = "v18";
+        vasja19.name = "v19";
+        vasja20.name = "v20";
+        vasja21.name = "v21";
+        vasja22.name = "v22";
+        vasja23.name = "v23";
+        vasja24.name = "v24";
+        vasja25.name = "v25";
+        vasja26.name = "v26";
+        vasja27.name = "v27";
     }
 
     public static class Zerg
