8c26d9f285df70dc87c2cca79c3400154b263998
Xiodane <Ilja098@mail.ru>
2016-01-06 22:45:45 +0200

    after

--- src/com/javarush/test/level03/lesson06/task01/Solution.java
+++ src/com/javarush/test/level03/lesson06/task01/Solution.java
@@ -12,7 +12,13 @@ public class Solution
 {
     public static void main(String[] args)
     {
-        //напишите тут ваш код
+        String[] array = new String[]{"Мама","Мыла","Раму"};
+        System.out.println(array[0]+array[1]+array[2]);
+        System.out.println(array[0]+array[2]+array[1]);
+        System.out.println(array[1]+array[2]+array[0]);
+        System.out.println(array[1]+array[0]+array[2]);
+        System.out.println(array[2]+array[1]+array[0]);
+        System.out.println(array[2]+array[0]+array[1]);
 
     }
 }
