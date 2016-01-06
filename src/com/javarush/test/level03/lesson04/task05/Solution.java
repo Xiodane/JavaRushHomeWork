78414505387254e4b7b55285721a26210e692d59
Xiodane <Ilja098@mail.ru>
2016-01-06 20:50:51 +0200

    after

---
+++ src/com/javarush/test/level03/lesson04/task05/Solution.java
@@ -0,0 +1,74 @@
+package com.javarush.test.level03.lesson04.task05;
+
+/* Сумма 10 чисел
+Вывести на экран сумму чисел от 1 до 10 построчно (должно быть 10 строк):
+1
+1+2=3
+1+2+3=6
+1+2+3+4=10
+...
+Пример вывода:
+1
+3
+6
+10
+...
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+    int result = 0;
+        for (int i = 1; i <2 ; i++)
+        {
+        result = result + i;
+        }    //напишите тут ваш код
+        System.out.println(result);
+        for (int i = 1; i <3 ; i++)
+        {
+            result = result + i;
+        }    //напишите тут ваш код
+        System.out.println(result);
+        for (int i = 1; i <4 ; i++)
+        {
+            result = result + i;
+        }    //напишите тут ваш код
+        System.out.println(result);
+        for (int i = 1; i <5 ; i++)
+        {
+            result = result + i;
+        }    //напишите тут ваш код
+        System.out.println(result);
+        for (int i = 1; i <6 ; i++)
+        {
+            result = result + i;
+        }    //напишите тут ваш код
+        System.out.println(result);
+        for (int i = 1; i <7 ; i++)
+        {
+            result = result + i;
+        }    //напишите тут ваш код
+        System.out.println(result);
+        for (int i = 1; i <8 ; i++)
+        {
+            result = result + i;
+        }    //напишите тут ваш код
+        System.out.println(result);
+        for (int i = 1; i < 9; i++)
+        {
+            result = result + i;
+        }    //напишите тут ваш код
+        System.out.println(result);
+        for (int i = 1; i <10 ; i++)
+        {
+            result = result + i;
+        }    //напишите тут ваш код
+        System.out.println(result);
+        for (int i = 1; i <11 ; i++)
+        {
+            result = result + i;
+        }    //напишите тут ваш код
+        System.out.println(result);
+    }
+}
