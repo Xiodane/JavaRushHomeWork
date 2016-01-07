000494dfb1bff4f7019bb5a5be6c3432b701e206
Xiodane <Ilja098@mail.ru>
2016-01-07 12:40:38 +0200

    after

--- src/com/javarush/test/level03/lesson08/task04/Solution.java
+++ src/com/javarush/test/level03/lesson08/task04/Solution.java
@@ -13,7 +13,10 @@ public class Solution
 {
     public static void main(String[] args) throws Exception
     {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine();
+        String name1 = reader.readLine();
+        System.out.println(name + " проспонсировал " + name1 + ", и она стала известной певицей.");
 
     }
 }
