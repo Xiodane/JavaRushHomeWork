df3c547473ff7fe2cec8e6846b933386aa8b2030
Xiodane <Ilja098@mail.ru>
2016-01-07 12:32:01 +0200

    after

--- src/com/javarush/test/level03/lesson08/task01/Solution.java
+++ src/com/javarush/test/level03/lesson08/task01/Solution.java
@@ -15,8 +15,8 @@ public class Solution
     public static void main(String[] args) throws Exception
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String name = reader.readLine();
         String age = reader.readLine();
-        System.out.println(name +" захватит мир через " + age + " лет. Му-ха-ха!" );
+        String name = reader.readLine();
+        System.out.println( name +" захватит мир через " + age + " лет. Му-ха-ха!" );
     }
 }
